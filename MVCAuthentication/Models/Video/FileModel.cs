baa31f7a1228bb563b1f10727c9fbceab7c11ed5
Dan89-L <denillimbu18@hotmail.com>
2019-02-21 21:17:39 +0000

    Update Video Features inside the same project

---
+++ MVCAuthentication/Models/Video/FileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAuthentication.Models.Video
+{
+    public class FileModel
+    {
+        public int invoke { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContentType { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
